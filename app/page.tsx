3bf55fc2932280648e9c5442f438783cb23b499f
Jacqueline Hubbard <19712939+JackieDev@users.noreply.github.com>
2026-04-03 10:07:36 +0100

    Added payment functionality and optimized for Mobile

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default async function Home() {
               Sign In
             </button>
           </SignInButton>
-          <SignUpButton mode="modal">
+          <SignUpButton mode="modal" forceRedirectUrl="/billing">
             <button className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-[1.65rem] text-[0.91875rem] font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50">
               Sign Up
             </button>
